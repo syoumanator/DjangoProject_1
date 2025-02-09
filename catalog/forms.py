40a97477fead50145d207e5dc5700578349a5d37
Alik <alik220201@gmail.com>
2025-02-09 23:30:54 +0700

    fixes and forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,43 @@
 import os
 
-from django import forms
-
 from catalog.models import Product
 
+from django import forms
+from dotenv import load_dotenv
+load_dotenv()
+FORBIDDEN_WORDS = os.getenv('FORBIDDEN_WORDS').split(', ')
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.BooleanField):
+                field.widget.attrs.update({"class": "form-check-input"})
+            else:
+                field.widget.attrs.update({"class": "form-control"})
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'category', 'image', 'price']
+        # fields = ["name", "description", "price", "image", "category", "is_publish"]
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price < 0:
+            raise forms.ValidationError('Цена продукта не может быть отрицательной.')
+        return price
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if any(word in name.lower() for word in FORBIDDEN_WORDS):
+            raise forms.ValidationError('Имя продукта содержит запрещенные слова.')
+        return name
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if any(word in description.lower() for word in FORBIDDEN_WORDS):
+            raise forms.ValidationError('Описание продукта содержит запрещенные слова.')
+        return description
--- catalog/views.py
+++ catalog/views.py
@@ -7,22 +7,24 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.urls import reverse_lazy
 
+from .forms import ProductForm
+
 
 class ContactDetailView(TemplateView):
     template_name = "catalog/contacts.html"
 
 
-def contact(request):
-    if request.method == 'POST':
-        # Получение данных из формы
-        name = request.POST.get('name')
-        message = request.POST.get('message')
-        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
-        print(name)
-        print(message)
-        # Здесь мы просто возвращаем простой ответ
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'contact.html')
+# def contact(request):
+#     if request.method == 'POST':
+#         # Получение данных из формы
+#         name = request.POST.get('name')
+#         message = request.POST.get('message')
+#         # Обработка данных (например, сохранение в БД, отправка email и т. д.)
+#         print(name)
+#         print(message)
+#         # Здесь мы просто возвращаем простой ответ
+#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+#     return render(request, 'contact.html')
 
 
 class ProductListView(ListView):
@@ -35,16 +37,19 @@ class ProductDetailView(DetailView):
 
 class ProductCreateView(CreateView):
     model = Product
-    fields = ('name', 'description', 'category', 'image', 'price')
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:catalog_list')
 
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = ('name', 'description', 'category', 'image', 'price')
+    form_class = ProductForm
     success_url = reverse_lazy('catalog:catalog_list')
 
 
 class ProductDeleteView(DeleteView):
     model = Product
+    template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:catalog_list')
+    context_object_name = 'product'
